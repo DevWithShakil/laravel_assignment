11aeee6a3c88aace33df269acd8e7d4636cd9f13
DevWithShakil <m.shakil720@gmail.com>
2025-10-27 14:24:03 +0600

    blade conversion completed successfully

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,28 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+// Home Page (pointing to pages/index.blade.php)
 Route::get('/', function () {
+    return view('pages.index');
+})->name('home');
+
+// Resume Page (pointing to pages/resume.blade.php)
+Route::get('/resume', function () {
+    return view('pages.resume');
+})->name('resume');
+
+// Projects Page (pointing to pages/projects.blade.php)
+Route::get('/projects', function () {
+    return view('pages.projects');
+})->name('projects');
+
+// Contact Page (pointing to pages/contact.blade.php)
+Route::get('/contact', function () {
+    return view('pages.contact');
+})->name('contact');
+
+// If views/welcome.blade.php exists and you need it:
+Route::get('/welcome', function () {
     return view('welcome');
 });
